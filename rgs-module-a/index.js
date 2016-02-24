3ac9ab22b935aa04842f958e6c924f774d4d5020
Rubén Guerrero <yo@rubenguerrero.com>
2016-02-24 22:29:16 +0100

    first commit

---
+++ rgs-module-a/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+	m1: function(){
+		console.log('Hello from m1');
+	},
+
+	m2: function(){
+		console.log('Hello from m2');
+	},
+
+	m3: function(){
+		return 'Hello World!';
+	}
+
+};
---
+++ rgs-module-b/index.js
@@ -0,0 +1,6 @@
+var req = require('rgs-module-a');
+
+req.m1();
+req.m2();
+
+console.log(req.m3());
